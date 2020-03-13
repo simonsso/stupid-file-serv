c4f5996b92a7f888b7806d5bb6e1ef0485a9dd85
Fredrik Simonsson <simson@thesimson.net>
2020-03-13 22:27:02 +0900

    Simple server

---
+++ flaskfs/__init__.py
@@ -0,0 +1,51 @@
+import os
+from flask import Flask
+import json
+
+globalfilesystem = {}
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+
+    # store all files in this global and forget all when retarted.
+    globalfilesystem = {}
+
+    #create some files present on restart.
+    globalfilesystem["index.html"] = "<body>"
+
+
+    @app.route('/files', methods=['GET'])
+    def hello():
+        response = {}
+        response ['files']= list (globalfilesystem.keys())
+        return json.JSONEncoder().encode(response)
+
+    @app.route('/files/<filename>', methods=['DELETE'] )
+    def deletefile(filename):
+        del globalfilesystem[filename]
+        return "File server:" + filename + "\r\n"
+
+    @app.route('/files/<filename>', methods=['POST'] )
+    def createfile(filename):
+        #Since there is no way of geting the data there is no need to store it
+        globalfilesystem[filename] = "dummydata"
+        return "File server created:" + filename + "\r\n" 
+
+    return app
