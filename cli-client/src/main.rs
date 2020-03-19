aaaf67d76c5f2d59ad268bbec4998522887feaa3
Fredrik Simonsson <simson@thesimson.net>
2020-03-19 23:08:08 +0900

    Serde parse files and reqwest

--- cli-client/src/main.rs
+++ cli-client/src/main.rs
@@ -4,58 +4,61 @@ use simple_error::SimpleError;
 #[macro_use]
 extern crate  clap;
 
-// use failure::ResultExt;
+extern crate serde;
 use exitfailure::ExitFailure;
 
-// pub enum Error<E> {
-//     UnexpectedError,
-//     UserError,
-//     OtherError(E)
+use std::collections::HashMap;
+
+#[macro_use]
+use serde::{Deserialize,Serialize};
+// #[derive(Deserialize)]
+// struct ListResponse {
+//     files: Vec<String>
 // }
+
 fn usage_error() -> Result<(), SimpleError>{
     Err(SimpleError::new("Usage error"))
 }
 
 fn delete_remote_file_command(remote_server:&str,filename:&str)-> Result<(), ExitFailure>
 {
-    // let url = remote_server.to_string()+"/files/"+&filename;
-    // // let rest = reqwest::blocking:: 
-    // // (&url)?;
+    let url = remote_server.to_string()+"/files/"+&filename;
 
+    let client = reqwest::blocking::Client::new();
+    let resp = client.delete(&url).send()?.error_for_status()?;
 
-    // let client = reqwest::Client::new();
-    // let res = client.delete(&url)
-    // .body("the exact body that is sent")
-    // .send();
-    // // res.w
     Ok(())
-
 }
-fn push_remote_file_command(remote_server:&str,filename:&str)-> Result<(), ExitFailure>
+fn push_remote_file_command(remote_server:&str,filename:&str,remotefilename:&str)-> Result<(), ExitFailure>
 {
     let url = remote_server.to_string()+"/files/"+&filename;
-    // let rest = reqwest::blocking:: 
-    // (&url)?; 
-
-    let x = async{
-        println!("Try push url {}",url);
-        let client = reqwest::Client::new();
-        let res = client.post(&url)
+    let client = reqwest::blocking::Client::new();
+    let resp = client.post(&url)
         .body("the exact body that is sent")
-        .send().await;
-        println!("Awaited!");
-        res
-    };
-    x.await();
+        .send()?;
+
+    resp.error_for_status()?;
     Ok(())
+}
+
+#[derive(Serialize, Deserialize)]
+struct Data{
+    files: Vec<String>
 }
 
 fn list_files_command(remote_server:&str)-> Result<(), ExitFailure>
 {
-    // let url = remote_server.to_string()+"files";
-    // println!("{}",url);
-    // let rest = reqwest::blocking::get(&url)?.text()?;
-    // println!("{}",rest);
+    let url = remote_server.to_string()+"files";
+    let client = reqwest::blocking::Client::new();
+    // let resp = client.get(&url).send()?.json::<HashMap<String,Vec<String>>>()?;
+    let resp = client.get(&url).send()?.json::<Data>()?;
+    for file in resp.files {
+        println!("{}",file);
+    }
+    // let files:Vec<String> = resp.entry("files") ;
+    // for (key,val) in resp.iter(){
+    //     println!("{}",key);
+    // }
     Ok(())
 }
 
@@ -65,9 +68,7 @@ fn main()-> Result<(), ExitFailure>  {
         (@setting SubcommandRequiredElseHelp)
         (author: "Author: Fredrik SIMONSSON")
         (about: "Command line tool for simple file server project")
-        // (@arg CONFIG: -c --config +takes_value "Sets a custom config file")
         (@arg SERVER: -s --server +takes_value "Remote server base url - default http://127.1:5000/")
-        // (@arg debug: -d ... "Sets the level of debugging information")
         (@subcommand list =>
             (about: "list files on server")
         )
@@ -98,8 +99,12 @@ fn main()-> Result<(), ExitFailure>  {
         },
         ("list",_args) =>{ list_files_command(server)?;},
         ("upload",Some(args)) =>{ 
-                println!("push");
-                push_remote_file_command(server,args.value_of("FILENAME").unwrap())?;
+                let localfilename = args.value_of("FILENAME").unwrap();
+                let remotefilename = match args.value_of("REMOTENAME") {
+                    Some(s) => {s}
+                    None => {localfilename}
+                };
+                push_remote_file_command(server,localfilename,remotefilename )?;
 
             },
         _ => { usage_error()?; }
